2c2b04b1a4a16127e051484f7d70dd4585a60701
perry <u21303759@utp.edu.pe>
2026-01-05 13:05:51 -0500

    url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dntkjaoyywbdreuqopda.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRudGtqYW95eXdiZHJldXFvcGRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNDc4NjMsImV4cCI6MjA3NzgyMzg2M30.4dx8avzfcU2aNEXesoOuMygmh3Go8KIvqRsepx0s5EY'
+const supabaseUrl = 'https://cvvgwcfjhcvylifxpadc.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2dmd3Y2ZqaGN2eWxpZnhwYWRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2Mjk2MTgsImV4cCI6MjA4MzIwNTYxOH0.lrAg7JynbNoV4eozr4wyDI2RfxQlbMWktcpoYt6DgDk'
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables')
